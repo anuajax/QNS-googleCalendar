178a494ac7ed2bc4b3d92496b09bbd6e2128f518
2017uec1581@mnit.ac.in <2017uec1581@mnit.ac.in>
2020-04-26 20:57:40 +0530

    cookies

--- app.js
+++ app.js
@@ -17,6 +17,9 @@ app.use(
       resave: false,
       secret: 'sssh, quiet! it\'s a secret!',
       cookie: {
+        path: "/auth/success",
+        maxAge: 1000 * 60 * 60 * 2,
+        sameSite: true,
         httpOnly: true,
         secure: true
       }
